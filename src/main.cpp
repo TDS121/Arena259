d87433600b2ba1cc7466265a2ce26bb77cebe86c
grumpywizardstudent <kleina@sou.edu>
2026-05-18 15:00:07 -0700

    reverting main.cpp changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include "arena.h"
 #include "creature.h"
-#include "./creatures/grumpywizard.h"
+#include "./creatures/sumo_sally.h"
 #include "./creatures/giantleech.h"
 
 int main()
 {
-    GrumpyWizard c1;
+    Creature c1(make_sumo_sally());
     Creature c2(makeGiantleech());
     
     std::cout << "Total creatures created: " << Creature::getCreatureCount() << std::endl; // print out the number of creatures in the arena
