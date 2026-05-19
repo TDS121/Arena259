4bf75b829da71ae3930b66154bd1dbbcadc16cf6
Tristan Skerritt <tds@Tristans-MacBook-Pro.local>
2026-05-18 17:41:16 -0700

    just 2

--- src/creatures/clawdragon.cpp
+++ src/creatures/clawdragon.cpp
@@ -9,11 +9,11 @@ Clawdragon::Clawdragon()
     : Creature("Clawdragon", 101, 18){
     }
 
+    
 void Clawdragon::specialMove(Creature& other){
     int slashDamage = RNG::randomValue(10, 25);
     std::cout << getName() << " slashes " << other.getName() << " with claws for "
               << slashDamage << " damage" << std::endl;
     other.takeDamage(slashDamage);
     incDamageDealt(slashDamage);
-    
 }
